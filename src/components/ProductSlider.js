55df36706d9d05cb70fc9ec2a0f4e0715faabef2
Yisal Khan <yisal94@gmail.com>
2024-11-09 00:29:28 +0500

    did required changes

--- src/components/ProductSlider.js
+++ src/components/ProductSlider.js
@@ -2,13 +2,15 @@ import Image from 'next/image';
 
 export default function ProductSlider({ productData }) {
   return (
-    <>
+    <div className="product-slider">
       <h3>{productData.heading}</h3>
       <div className="product-card-wrapper">
         {productData.products && productData.products.map(product => (
             <div className="product-card">
               <p className="product-detail">{product.name}</p>
-              <Image src={product.image} alt="Feature Product" />
+              <div className="slider-image">
+                <Image src={product.image} alt="Feature Product" height={400} />
+              </div>
               <div className="btn-wrapper">
                 <button className="btn btn-dark" type="button">Learn More</button>
                 <button className="btn btn-outline-gray" type="button">Get Started</button>
@@ -16,6 +18,6 @@ export default function ProductSlider({ productData }) {
             </div>
         ))}
       </div>
-    </>
+    </div>
   );
 }
